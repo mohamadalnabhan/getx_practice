0acff8ebc135dc28bc97b558eb723487725424ed
mohamadalnabhan <mhamadnabhan6@gmail.com>
2025-04-14 21:42:08 +0300

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_practice/malware_pra/admin.dart';
+import 'package:getx_practice/malware_pra/home_mal.dart';
+import 'package:getx_practice/malware_pra/login.dart';
+import 'package:getx_practice/malware_pra/malware/auth_middlware.dart';
 import 'package:getx_practice/previous_lessons/dependency_injection/home_inj.dart';
 import 'package:getx_practice/previous_lessons/getx_obx/home_go.dart';
 import 'package:getx_practice/utils/Mybindings.dart';
 import 'package:getx_practice/view/home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+SharedPreferences? sharedPref;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  sharedPref = await SharedPreferences.getInstance();
+
   runApp(const MyApp());
 }
 
@@ -16,13 +25,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-     debugShowCheckedModeBanner: false,
-     home: HomeInj(),
-     initialBinding: Mybindings(),
-     initialRoute: "/",
-     getPages: [
-      GetPage(name: "/", page: ()=> HomeInj()),
-     ],
+      debugShowCheckedModeBanner: false,
+
+      initialBinding: Mybindings(),
+      initialRoute: "/",
+      getPages: [
+         GetPage(name: "/home", page: () => HomeMal()),
+        GetPage(name: "/", page: () => Login() ,middlewares: [
+          AuthMiddlware()
+        ]),
+        GetPage(name: "/admin", page: () => Admin()),
+      ],
     );
   }
 }
